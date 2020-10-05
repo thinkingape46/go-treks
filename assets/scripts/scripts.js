77b4aee0f5a963b9f3501a501f58607ca85ebc3a
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-10-05 22:55:33 +0530

    changed the banner section

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,11 +1,14 @@
 let bannerImage = document.getElementById("banner-img");
 let header = document.getElementById("header");
+let logoContainer = document.getElementById("logo-container");
 
 document.addEventListener("scroll", () => {
     if (bannerImage.getBoundingClientRect().top <= -100) {
-        header.classList.add("header_dark");
+        header.classList.add("header--scrolled");
+        logoContainer.classList.add("logo-container--scrolled");
     }
     else {
-        header.classList.remove("header_dark");
+        header.classList.remove("header--scrolled");
+        logoContainer.classList.remove("logo-container--scrolled");
     }
 })
