f074cde6c0945840e24cdacb7abaf571c82eebc4
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-10-06 17:11:17 +0530

    working on features section

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,11 @@
 let bannerImage = document.getElementById("banner-img");
 let header = document.getElementById("header");
 let logoContainer = document.getElementById("logo-container");
+let transportContainer = document.getElementsByClassName("feature_transport");
+let wheel = document.getElementsByClassName("feature_icon_wheel");
+let foodfeature = document.getElementsByClassName("feature_food");
+let fork = document.getElementsByClassName("feature_icon_fork");
+let spoon = document.getElementsByClassName("feature_icon_spoon");
 
 document.addEventListener("scroll", () => {
     if (bannerImage.getBoundingClientRect().top <= -100) {
@@ -11,4 +16,19 @@ document.addEventListener("scroll", () => {
         header.classList.remove("header--scrolled");
         logoContainer.classList.remove("logo-container--scrolled");
     }
+})
+
+foodfeature[0].addEventListener('mouseover', () => {
+    fork[0].classList.add("feature_icon_fork--spin");
+    spoon[0].classList.add("feature_icon_spoon--spin");
+})
+foodfeature[0].addEventListener('mouseout', () => {
+    fork[0].classList.remove("feature_icon_fork--spin");
+    spoon[0].classList.remove("feature_icon_spoon--spin");
+})
+transportContainer[0].addEventListener('mouseover', () => {
+    wheel[0].classList.add("feature_icon_wheel--spin");
+})
+transportContainer[0].addEventListener('mouseout', () => {
+    wheel[0].classList.remove("feature_icon_wheel--spin");
 })
